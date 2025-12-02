307cb9ba019c64ce402810d4ce018c879e3ff046
ajaykumawat09 <kumawatajay137@gmail.com>
2025-12-02 15:28:51 +0530

    Header

---
+++ src/components/Drawer.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import Link from "next/link";
+import { IconMenu2 } from "@tabler/icons-react";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetDescription, SheetTrigger } from "@/components/ui/sheet";
+
+const Drawer = () => {
+  return (
+    <header className="w-full bg-white shadow-sm">
+      <div className="max-w-7xl mx-auto flex items-center justify-between py-3 px-3">
+        <div className="w-10 h-10 bg-blue-600 rounded-xl flex items-center justify-center text-white font-bold text-xl">
+          S
+        </div>
+
+        <Sheet>
+          <SheetTrigger>
+            <div className="w-12 h-12 border rounded-2xl border-blue-500 flex items-center justify-center hover:bg-blue-50 transition">
+              <IconMenu2 size={22} />
+            </div>
+          </SheetTrigger>
+
+          <SheetContent side="right" className="p-5">
+
+           
+            <SheetHeader>
+              <SheetTitle></SheetTitle>
+              <SheetDescription >
+              </SheetDescription>
+            </SheetHeader>
+
+
+            <div className=" flex flex-col gap-5">
+              <Link href="/" className="text-lg font-semibold">
+                Home
+              </Link>
+
+              <Link href="/Categories" className="text-lg text-gray-500">
+                Categories
+              </Link>
+
+              <Link href="/Products" className="text-lg text-gray-500">
+                Products
+              </Link>
+            </div>
+
+          </SheetContent>
+        </Sheet>
+
+      </div>
+    </header>
+  );
+};
+
+export default Drawer;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { IconSearch } from "@tabler/icons-react";
+import Drawer from "./Drawer";
+
+
+
 
 const Header = () => {
-    return (
-        <div>Header</div>
-    )
-}
+  return (
+    <>
+    <header className="w-full bg-white shadow-sm  max-sm:hidden">
+      <div className="max-w-7xl mx-auto flex items-center justify-between py-3 px-4">
 
-export default Header
+        {/* -------- LEFT SIDE (Logo + Navbar) -------- */}
+        <div className="flex items-center gap-10">
+          
+          {/* Logo */}
+          <div className="flex items-center gap-3">
+            <div className="w-10 h-10 bg-blue-600 rounded-xl flex items-center justify-center text-white font-bold text-xl">
+              S
+            </div>
+            <h1 className="text-xl font-bold">SCOTT Parts</h1>
+          </div>
+
+          {/* Navbar */}
+          <nav className="flex items-center gap-8 font-semibold text-gray-600">
+            <Link href="/" className="hover:text-black transition">Home</Link>
+            <Link href="/Categories" className="hover:text-black transition">Categories</Link>
+            <Link href="/Products" className="hover:text-black transition">Products</Link>
+          </nav>
+
+        </div>
+
+    
+        <div className="flex items-center bg-gray-100 px-4 py-2 rounded-full w-60 gap-2">
+          <IconSearch size={20} className="text-gray-500" />
+          
+          <input
+            type="text"
+            placeholder="Search"
+            className="bg-transparent focus:outline-none text-gray-600 w-full"
+          />
+        </div>
+      
+      </div>
+   
+    </header>
+       <div
+      className="md:hidden"><Drawer/></div>
+      </>
+  );
+};
+
+export default Header;
